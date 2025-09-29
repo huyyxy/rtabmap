bdc5912662147a2bc68a510ee9aa1e6037c7ffa3
huyyxy <huyyxy@163.com>
2025-09-29 14:41:00 +0800

    fix: 修复若干错误

--- python_binding/src/parameters_binding.cpp
+++ python_binding/src/parameters_binding.cpp
@@ -6,80 +6,64 @@
 #include <pybind11/stl.h>
 
 #include <rtabmap/core/Parameters.h>
+#include <rtabmap/utilite/UConversion.h>
 
 namespace py = pybind11;
 
 void init_parameters(py::module &m) {
-    py::class_<rtabmap::Parameters>(m, "Parameters")
-        .def(py::init<>(), "Default constructor")
-        
-        // Static methods for parameter access
-        .def_static("getDefaultParameters", &rtabmap::Parameters::getDefaultParameters,
-                   "Get all default parameters as a dictionary")
-        .def_static("getDefaultParameter", &rtabmap::Parameters::getDefaultParameter,
-                   "Get default value for a parameter", py::arg("key"))
-        .def_static("getType", &rtabmap::Parameters::getType,
-                   "Get parameter type", py::arg("key"))
-        .def_static("getDescription", &rtabmap::Parameters::getDescription,
-                   "Get parameter description", py::arg("key"))
-        
-        // Parameter validation
-        .def_static("isFeatureParameter", &rtabmap::Parameters::isFeatureParameter,
-                   "Check if parameter is a feature parameter", py::arg("key"))
-        .def_static("isBowParameter", &rtabmap::Parameters::isBowParameter,
-                   "Check if parameter is a BoW parameter", py::arg("key"))
-        .def_static("isIcpParameter", &rtabmap::Parameters::isIcpParameter,
-                   "Check if parameter is an ICP parameter", py::arg("key"))
-        .def_static("isStereoParameter", &rtabmap::Parameters::isStereoParameter,
-                   "Check if parameter is a stereo parameter", py::arg("key"))
-        
-        // Parameter parsing and validation
-        .def_static("parse", py::overload_cast<const std::map<std::string, std::string>&>(&rtabmap::Parameters::parse),
-                   "Parse and validate parameters", py::arg("parameters"))
-        .def_static("parseArguments", &rtabmap::Parameters::parseArguments,
-                   "Parse command line arguments", py::arg("argc"), py::arg("argv"), py::arg("remove_processed") = true)
-        
-        // Utility methods for parameter conversion
-        .def_static("uStr2Bool", &rtabmap::Parameters::uStr2Bool,
-                   "Convert string to boolean", py::arg("str"))
-        .def_static("uBool2Str", &rtabmap::Parameters::uBool2Str,
-                   "Convert boolean to string", py::arg("boolean"))
-        .def_static("uStr2Int", &rtabmap::Parameters::uStr2Int,
-                   "Convert string to integer", py::arg("str"))
-        .def_static("uInt2Str", &rtabmap::Parameters::uInt2Str,
-                   "Convert integer to string", py::arg("integer"))
-        .def_static("uStr2Float", &rtabmap::Parameters::uStr2Float,
-                   "Convert string to float", py::arg("str"))
-        .def_static("uFloat2Str", &rtabmap::Parameters::uFloat2Str,
-                   "Convert float to string", py::arg("value"))
-        
-        // Parameter groups for easier access
-        .def_static("getDefaultParametersRtabmap", &rtabmap::Parameters::getDefaultParametersRtabmap,
-                   "Get default RTAB-Map core parameters")
-        .def_static("getDefaultParametersCamera", &rtabmap::Parameters::getDefaultParametersCamera,
-                   "Get default camera parameters")
-        .def_static("getDefaultParametersOdometry", &rtabmap::Parameters::getDefaultParametersOdometry,
-                   "Get default odometry parameters")
-        .def_static("getDefaultParametersStereo", &rtabmap::Parameters::getDefaultParametersStereo,
-                   "Get default stereo parameters")
-        .def_static("getDefaultParametersRegistration", &rtabmap::Parameters::getDefaultParametersRegistration,
-                   "Get default registration parameters");
+    // Note: Parameters class has private constructor, so we expose only static methods
+    py::module params = m.def_submodule("Parameters", "RTAB-Map Parameters static methods");
+    
+    // Static methods for parameter access
+    params.def("getDefaultParameters", &rtabmap::Parameters::getDefaultParameters,
+               "Get all default parameters as a dictionary",
+               py::return_value_policy::reference_internal);
+    params.def("getType", &rtabmap::Parameters::getType,
+               "Get parameter type", py::arg("key"));
+    params.def("getDescription", &rtabmap::Parameters::getDescription,
+               "Get parameter description", py::arg("key"));
+    
+    // Parameter validation
+    params.def("isFeatureParameter", &rtabmap::Parameters::isFeatureParameter,
+               "Check if parameter is a feature parameter", py::arg("param"));
+    
+    // Parameter parsing and validation
+    params.def("parseArguments", &rtabmap::Parameters::parseArguments,
+               "Parse command line arguments", py::arg("argc"), py::arg("argv"), py::arg("onlyParameters") = false);
+    
+    // Parameter groups for easier access
+    params.def("getDefaultOdometryParameters", &rtabmap::Parameters::getDefaultOdometryParameters,
+               "Get default odometry parameters", py::arg("stereo") = false, py::arg("vis") = true, py::arg("icp") = false);
+    params.def("getDefaultParameters", py::overload_cast<const std::string&>(&rtabmap::Parameters::getDefaultParameters),
+               "Get default parameters for a specific group", py::arg("group"));
+    
+    // Utility functions from UConversion (not Parameters class methods)
+    py::module utils = m.def_submodule("Utils", "Utility conversion functions");
+    utils.def("uStr2Bool", py::overload_cast<const std::string&>(&uStr2Bool),
+              "Convert string to boolean", py::arg("str"));
+    utils.def("uBool2Str", &uBool2Str,
+              "Convert boolean to string", py::arg("boolean"));
+    utils.def("uStr2Int", &uStr2Int,
+              "Convert string to integer", py::arg("str"));
+    utils.def("uNumber2Str", py::overload_cast<int>(&uNumber2Str),
+              "Convert integer to string", py::arg("number"));
+    utils.def("uStr2Float", &uStr2Float,
+              "Convert string to float", py::arg("str"));
+    utils.def("uNumber2Str", py::overload_cast<float, int, bool>(&uNumber2Str),
+              "Convert float to string", py::arg("number"), py::arg("precision") = 6, py::arg("fixed") = false);
     
     // Add parameter constants as module attributes
     py::module param = m.def_submodule("Param", "RTAB-Map parameter constants");
     
-    // Core RTAB-Map parameters
-    param.attr("kRtabmapPublishStats") = rtabmap::Parameters::kRtabmapPublishStats();
-    param.attr("kRtabmapPublishLastSignature") = rtabmap::Parameters::kRtabmapPublishLastSignature();
-    param.attr("kRtabmapPublishPdf") = rtabmap::Parameters::kRtabmapPublishPdf();
-    param.attr("kRtabmapPublishLikelihood") = rtabmap::Parameters::kRtabmapPublishLikelihood();
+    // Core RTAB-Map parameters (only include ones that actually exist)
     param.attr("kRtabmapTimeThr") = rtabmap::Parameters::kRtabmapTimeThr();
     param.attr("kRtabmapMemoryThr") = rtabmap::Parameters::kRtabmapMemoryThr();
     param.attr("kRtabmapDetectionRate") = rtabmap::Parameters::kRtabmapDetectionRate();
     param.attr("kRtabmapImageBufferSize") = rtabmap::Parameters::kRtabmapImageBufferSize();
     param.attr("kRtabmapMaxRetrieved") = rtabmap::Parameters::kRtabmapMaxRetrieved();
-    param.attr("kRtabmapStatisticLogged") = rtabmap::Parameters::kRtabmapStatisticLogged();
-    param.attr("kRtabmapStatisticLoggedHeaders") = rtabmap::Parameters::kRtabmapStatisticLoggedHeaders();
+    param.attr("kRtabmapLoopThr") = rtabmap::Parameters::kRtabmapLoopThr();
+    param.attr("kRtabmapLoopRatio") = rtabmap::Parameters::kRtabmapLoopRatio();
+    param.attr("kRtabmapLoopGPS") = rtabmap::Parameters::kRtabmapLoopGPS();
     
     // Memory parameters
     param.attr("kMemRehearsalSimilarity") = rtabmap::Parameters::kMemRehearsalSimilarity();
@@ -88,10 +72,6 @@ void init_parameters(py::module &m) {
     param.attr("kMemRawDescriptorsKept") = rtabmap::Parameters::kMemRawDescriptorsKept();
     param.attr("kMemSaveDepth16Format") = rtabmap::Parameters::kMemSaveDepth16Format();
     param.attr("kMemCompressionParallelized") = rtabmap::Parameters::kMemCompressionParallelized();
-    param.attr("kMemLaserScanDownsampleStepSize") = rtabmap::Parameters::kMemLaserScanDownsampleStepSize();
-    param.attr("kMemLaserScanVoxelSize") = rtabmap::Parameters::kMemLaserScanVoxelSize();
-    param.attr("kMemLaserScanNormalK") = rtabmap::Parameters::kMemLaserScanNormalK();
-    param.attr("kMemLaserScanNormalRadius") = rtabmap::Parameters::kMemLaserScanNormalRadius();
     
     // RGB-D parameters
     param.attr("kRGBDEnabled") = rtabmap::Parameters::kRGBDEnabled();
@@ -102,28 +82,9 @@ void init_parameters(py::module &m) {
     param.attr("kRGBDNewMapOdomChangeDistance") = rtabmap::Parameters::kRGBDNewMapOdomChangeDistance();
     param.attr("kRGBDOptimizeFromGraphEnd") = rtabmap::Parameters::kRGBDOptimizeFromGraphEnd();
     param.attr("kRGBDOptimizeMaxError") = rtabmap::Parameters::kRGBDOptimizeMaxError();
-    param.attr("kRGBDSavedLocalizationIgnored") = rtabmap::Parameters::kRGBDSavedLocalizationIgnored();
     param.attr("kRGBDStartAtOrigin") = rtabmap::Parameters::kRGBDStartAtOrigin();
     param.attr("kRGBDGoalReachedRadius") = rtabmap::Parameters::kRGBDGoalReachedRadius();
-    param.attr("kRGBDGoalsSavedInUserData") = rtabmap::Parameters::kRGBDGoalsSavedInUserData();
-    param.attr("kRGBDMaxLocalRetrieved") = rtabmap::Parameters::kRGBDMaxLocalRetrieved();
-    param.attr("kRGBDProximityByTime") = rtabmap::Parameters::kRGBDProximityByTime();
-    param.attr("kRGBDProximityBySpace") = rtabmap::Parameters::kRGBDProximityBySpace();
-    param.attr("kRGBDProximityMaxGraphDepth") = rtabmap::Parameters::kRGBDProximityMaxGraphDepth();
-    param.attr("kRGBDProximityPathMaxNeighbors") = rtabmap::Parameters::kRGBDProximityPathMaxNeighbors();
-    param.attr("kRGBDProximityPathFilteringRadius") = rtabmap::Parameters::kRGBDProximityPathFilteringRadius();
-    param.attr("kRGBDProximityPathRawPosesUsed") = rtabmap::Parameters::kRGBDProximityPathRawPosesUsed();
-    param.attr("kRGBDProximityAngle") = rtabmap::Parameters::kRGBDProximityAngle();
-    param.attr("kRGBDScanMatchingIdsSavedInLinks") = rtabmap::Parameters::kRGBDScanMatchingIdsSavedInLinks();
-    param.attr("kRGBDLocalRadius") = rtabmap::Parameters::kRGBDLocalRadius();
-    param.attr("kRGBDLocalImmunizationRatio") = rtabmap::Parameters::kRGBDLocalImmunizationRatio();
-    param.attr("kRGBDInvertedReg") = rtabmap::Parameters::kRGBDInvertedReg();
     param.attr("kRGBDForceOdom3DoF") = rtabmap::Parameters::kRGBDForceOdom3DoF();
-    
-    // Loop closure parameters
-    param.attr("kRtabmapLoopThr") = rtabmap::Parameters::kRtabmapLoopThr();
-    param.attr("kRtabmapLoopRatio") = rtabmap::Parameters::kRtabmapLoopRatio();
-    param.attr("kRtabmapLoopGPS") = rtabmap::Parameters::kRtabmapLoopGPS();
     
     // Feature detection parameters
     param.attr("kKpMaxFeatures") = rtabmap::Parameters::kKpMaxFeatures();
